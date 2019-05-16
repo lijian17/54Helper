8b40e37e4364b1c93e967977d168ceb30d5b6ebb
深情小建 <374452668@qq.com>
2019-05-16 16:48:30 +0800

    初步完成

--- apps/google-assistant/index.js
+++ apps/google-assistant/index.js
@@ -1,50 +1,84 @@
 /**
- * 54Helper 信息编解码
+ * 54Helper 谷歌访问助手
  */
 new Vue({
 	el: '#pageContainer',
 	data: {
-		selectedType: 'coreValuesEncode',
-		sourceContent: '',
-		resultContent: ''
+		iii: null,
+		bgPage: null,
+		popupMessage: '交流QQ群：216966558',
+		offStatus: false,
+		onStatus: false,
+		tips: '',
+		startP: false,
+		stopP: false
+	},
+
+	created: function() {
+		this.bgPage = chrome.extension.getBackgroundPage();
 	},
 
 	mounted: function() {
-		let MSG_TYPE = Tarp.require('../static/js/msg_type');
-
-		// 在tab创建或者更新时候，监听事件，看看是否有参数传递过来
-		chrome.runtime.onMessage.addListener((request, sender, callback) => {
-			if(request.type === MSG_TYPE.TAB_CREATED_OR_UPDATED && request.event === MSG_TYPE.EN_DECODE) {
-				if(request.content) {
-					this.sourceContent = request.content;
-					this.convert();
-				}
-			}
-		});
-
-		this.$refs.srcText.focus();
+		this.show();
+		this.iii = setInterval(this.show, 1e3);
 	},
+
 	methods: {
-		convert: function() {
-			this.$nextTick(() => {
-
-				let tools = Tarp.require('./endecode-lib');
-
-				if(this.selectedType === 'coreValuesEncode') {
-					this.resultContent = tools.coreValuesEncode(this.sourceContent);
-				} else if(this.selectedType === 'coreValuesDecode') {
-					this.resultContent = tools.coreValuesDecode(this.sourceContent.replace(/\\U/g, '\\u'));
-				}
-			});
+		show: function() {
+			var e = this.bgPage.popupView;
+			console.log("show:" + JSON.stringify(e));
+			switch(e.status) {
+				case "off":
+					this.offStatus = true;
+					this.onStatus = false;
+					this.tips = e.warming;
+					break;
+				case "on":
+					this.offStatus = false;
+					this.onStatus = true;
+					break;
+				default:
+					console.error("这错误不可能")
+			}
+//			this.popupMessage = localStorage["popup-message"];
+			if(localStorage["stop-proxy"] && "true" == localStorage["stop-proxy"]) {
+				this.startP = true;
+				this.stopP = false;
+			} else {
+				this.startP = false;
+				this.stopP = true;
+			}
 		},
 
-		clear: function() {
-			this.sourceContent = '';
-			this.resultContent = '';
+		refreshF: function() {
+			chrome.runtime.sendMessage({
+				reqtype: "refresh"
+			}, function() {});
 		},
 
-		getResult: function() {
-			this.$refs.rstCode.select();
+		restartF: function() {
+			if(localStorage["auto-homepage"] && "true" == localStorage["auto-homepage"]) {
+				chrome.runtime.reload()
+			} else {
+				this.offStatus = true;
+				this.tips = "请你打开设置的主页，重启后检测<br>不到主页会激活失败！10秒后重启,请不要关闭页面";
+				window.clearInterval(this.iii);
+				setTimeout(function() {
+					chrome.runtime.reload()
+				}, 1e4);
+			}
+		},
+
+		stopF: function() {
+			chrome.runtime.sendMessage({
+				reqtype: "stop"
+			}, function() {});
+		},
+
+		startF: function() {
+			chrome.runtime.sendMessage({
+				reqtype: "start"
+			}, function() {});
 		}
 	}
 });
